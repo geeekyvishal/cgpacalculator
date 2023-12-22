c2544d90415e2c42735d75a0b7d8412a4a274abc
Vishal <141127867+geeekyvishal@users.noreply.github.com>
2023-12-22 15:34:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function calculateCGPA() {
+    const form = document.getElementById('cgpaForm');
+    const courses = form.querySelectorAll('.course input:checked');
+    let totalPoints = 0;
+    let totalCredits = 18;
+
+    courses.forEach(course => {
+        const credit = (course.value === "0") ? 0 : 4; // Assigning 0 credits for failed courses
+        // totalPoints += parseInt(course.value) * credit;
+        totalPoints += parseInt(course.value);
+        // totalCredits += credit;
+    });
+
+
+    const cgpa = totalPoints / totalCredits;
+    // const cgpa = (totalCredits === 0) ? 0 : totalPoints / totalCredits;
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = `<p>Overall CGPA: ${cgpa.toFixed(2)}</p>`;
+}
